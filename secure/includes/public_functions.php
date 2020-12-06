d2d6630ed2c3a2fd7f569b80e2af999b1dca5d02
Messner Daniel (Student Com19) <daniel.messner@stud-inf.unibz.it>
2020-12-06 18:33:07 +0100

    Open single post

--- secure/includes/public_functions.php
+++ secure/includes/public_functions.php
@@ -5,15 +5,34 @@
 function getPublishedPosts() {
 	// use global $conn object in function
 	global $mysqli;
+	$posts = null;
 
 	$sql = "SELECT * FROM posts WHERE published=true";
 	$result = $mysqli->query($sql);
 
 	// fetch all posts as an associative array called $posts
-	$posts = $result->fetch_all(MYSQLI_ASSOC);
+	if($result)
+		$posts = $result->fetch_all(MYSQLI_ASSOC);
 
 	return $posts;
 }
 
-// more functions to come here ...
+/* * * * * * * * * * * * * * *
+* Returns a single post
+* * * * * * * * * * * * * * */
+function getPost($slug){
+	global $mysqli;
+	$post = null;
+	// Get single post slug
+	$post_slug = $_GET['post-slug'];
+	$sql = "SELECT * FROM posts WHERE slug='$post_slug' AND published=true";
+	$result = $mysqli->query($sql);
+
+	// fetch query results as associative array.
+	if($result)
+		$post = $result->fetch_assoc();
+	
+	return $post;
+}
+
 ?>
---
+++ secure/single_post.php
@@ -0,0 +1,46 @@
+<?php
+// Initialize the session
+session_start();
+?>
+</head>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+   <?php include (dirname(__FILE__).'/components/head.php');?>
+   <?php require_once('config.php') ?>
+   <?php require_once(ROOT_PATH .'/includes/public_functions.php') ?>
+   <?php 
+	if (isset($_GET['post-slug'])) {
+		$post = getPost($_GET['post-slug']);
+	}
+    ?>
+    <title><?php echo $post['title'] ?></title>
+</head>
+<body>
+<?php include (dirname(__FILE__).'/components/navbar.php');?>
+<div class="container">
+	
+	<div class="content" >
+		<!-- Page wrapper -->
+		<div class="post-wrapper">
+			<!-- full post div -->
+			<div class="full-post-div">
+            <?php if ($post == null): ?>
+                <h2 class="post-title">Sorry... This post doesn`t exist</h2>
+			<?php elseif ($post['published'] == false): ?>
+                <h2 class="post-title">Sorry... This post has not been published</h2>
+			<?php else: ?>
+				<h2 class="post-title"><?php echo $post['title']; ?></h2>
+				<div class="post-body-div">
+					<?php echo html_entity_decode($post['body']); ?>
+				</div>
+			<?php endif ?>
+			</div>
+			<!-- // full post div -->
+			
+			<!-- comments section -->
+			<!--  coming soon ...  -->
+		</div>
+	</div>
+</div>
+<!-- // content -->
